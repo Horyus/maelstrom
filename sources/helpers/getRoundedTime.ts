db0d432daae33e6bcbfea45fa192cfd153cffb37
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-12-20 23:46:07 -0500

    Set live time to 10 minutes in past
      - Allows apis to properly gather informations
    
    MSTRM HF

--- sources/helpers/getRoundedTime.ts
+++ sources/helpers/getRoundedTime.ts
@@ -1,4 +1,4 @@
 export function getRoundedTime(time?: number): number {
-    const now = time || Date.now();
+    const now = time || Date.now() - (10 * 60 * 1000);
     return now - (now % (5 * 60 * 1000));
 }
