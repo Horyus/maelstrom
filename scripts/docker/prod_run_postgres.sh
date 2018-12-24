62e857f5af5fb4823a0dd4fc3d2b1ffa214c128f
Iulian Rotaru <iulian@rotaru.fr>
2018-12-23 18:12:59 -0600

    Prod DB listens on 0.0.0.0
    MSTRM HF

--- scripts/docker/prod_run_postgres.sh
+++ scripts/docker/prod_run_postgres.sh
@@ -3,10 +3,10 @@
 docker run \
     -d \
     --name maelstrom-db \
-    -p 127.0.0.1:5432:5432 \
+    -p 5432:5432 \
     --env POSTGRES_PASSWORD=pass \
     --env POSTGRES_USER=maelstrom \
     --env POSTGRES_DB=maelstrom \
-    --volume maelstrom_data:/var/lib/postgresql:rw \
+    --volume maelstrom_data:/var/lib/postgresql/data:rw \
     --restart always \
     postgres
